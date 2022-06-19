f7d1d36032a4cf7d53975a9d40d61f18f5592cee
Artyom <saaworking@gmail.com>
2022-06-19 14:07:12 +0300

    Timer

--- js/script.js
+++ js/script.js
@@ -38,4 +38,56 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	});
 
+
+	// Таймер
+
+	const deadline = '2022-07-15';
+
+	function getTimeRemaining() {
+		const time = new Date(deadline) - new Date(),
+			days = Math.floor(time / (1000 * 60 * 60 * 24)),
+			hours = Math.floor((time / (1000 * 60 * 60)) % 24),
+			minutes = Math.floor((time / (1000 * 60)) % 60),
+			seconds = Math.floor((time / 1000) % 60);
+
+		return {
+			time, days, hours, minutes, seconds
+		};
+	}
+
+
+	setTimer('.timer');
+
+	function setZero(num) {
+		return num < 10 ? `0${num}` : num;
+	}
+
+	function setTimer(selector) {
+		const timer = document.querySelector(selector),
+			timerDays = timer.querySelector('#days'),
+			timerHours = timer.querySelector('#hours'),
+			timerMinutes = timer.querySelector('#minutes'),
+			timerSeconds = timer.querySelector('#seconds'),
+			timerId = setInterval(updateTimer, 1000);
+
+			updateTimer();
+
+		function updateTimer() {
+			const { time, days, hours, minutes, seconds } = getTimeRemaining();
+			timerDays.textContent = setZero(days);
+			timerHours.textContent = setZero(hours);
+			timerMinutes.textContent = setZero(minutes);
+			timerSeconds.textContent = setZero(seconds);
+
+			if (time <= 0) {
+				clearInterval(timerId);
+				timerDays.textContent = '00';
+				timerHours.textContent = '00';
+				timerMinutes.textContent = '00';
+				timerSeconds.textContent = '00';
+			}
+		}
+
+	}
+
 });
